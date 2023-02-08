d9017b4a6c4d5a0b0d01f4db8036a2f66aaae6d9
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-02-08 01:54:05 +0000

    nav register page

--- lib/pages/registerpage.dart
+++ lib/pages/registerpage.dart
@@ -15,51 +15,153 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: 
-        Colors.white,
+        backgroundColor: Colors.white,
         elevation: 0,
-      leading: 
-       IconButton(
-         icon: const Icon(Icons.arrow_back),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
           color: Colors.black,
-         onPressed: () {},
-       ),
-
-      
-      
+          onPressed: () {},
+        ),
       ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset(
+                'images/groc.png',
+                width: 200,
+                height: 200,
+              ),
+            const Text("Let's you in",
+                style: TextStyle(
+                  fontSize: 33,
+                  fontWeight: FontWeight.bold,
+                )),
+
+            const SizedBox(
+
+              height: 20,
+            ),
+           Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: ElevatedButton.icon(onPressed: (){},
+                     style: 
+                   ElevatedButton.styleFrom(
+                     backgroundColor: Colors.white,
+                     foregroundColor: Colors.black,
+                     padding: 
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                    side: 
+                      const BorderSide(color: Colors.black12, width: 1),
+                      
+                     elevation: 0,
+                   ),
+             
+             
+             icon: Image.asset('images/G.png', width: 20, height: 20,),
+              
+                label: const Text("Continue with Google",
+                
+                style: TextStyle(
+                  fontSize: 10,
+                  
+                
+                ),
+                ),
+               
+                     
+                    
+             ),
+           ),
+
+           SizedBox(
+              height: 60,
+            ),
+
+           const Center(
+             child: Text("or",
+                  style: TextStyle(
+                    fontSize: 20,
+                  
+                  )),
+           ),
 
           
-        
-  
+          
+          //  style: ElevatedButton.styleFrom(
+          //    backgroundColor: Colors.white,
+          //    foregroundColor: Colors.black,
+          //    shape: RoundedRectangleBorder(
+          //      borderRadius: BorderRadius.circular(30),
     
-      body:
-   
+            SizedBox(
+              height: 80,
+            ),
 
+
+            ElevatedButton(onPressed: (){}, 
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 25),
+              side: const BorderSide(color: Colors.black12, width: 1),
+              elevation: 0,
+            ),
+            child: 
+            const Text("Sign in with Email and Password",
+            
+            style: TextStyle(
+              fontSize: 10,
+              
+      
+              
+ 
+            ),
+            ),
+            
+            
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+            
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-             
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Image.asset('images/groc.png',
-                  width: 200,
-                  height: 200,      
-                  ),
+                const Text("Don't have an account?",
+
+                style: TextStyle(
+                  fontSize: 10,
+                  
                 ),
+                ),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Sign up",
+                      ))
+            
               ],
             ),
+          
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+
+           
 
             
-         
       
-     
-   
-   
-    
-  
-    
-      
-    );
+            
+
+
   }
 }
