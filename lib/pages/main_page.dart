b3de8b70f1a81666fef3c7d96b1d554235fab0bc
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-02-10 00:36:31 +0000

    upload

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,3 +1,5 @@
+import 'package:fds/pages/customer.dart';
+import 'package:fds/pages/registerpage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -10,16 +12,31 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  List pages = [
+    const CustomerPage(),
+    const MyHomePage(),
+  ];
+  
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.amber,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.red,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
           ),
-          
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: 'Orders',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: 'Orders',
+          ),
           BottomNavigationBarItem(
             icon: Icon(Icons.shopping_cart_outlined),
             label: 'Orders',
