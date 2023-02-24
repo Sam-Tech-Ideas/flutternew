6a197e785add3e830002feef31f393019b855261
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-02-24 14:51:38 +0000

    trial success

---
+++ lib/widgets/cart_products.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/cart_controller.dart';
+import '../models/product_model.dart';
+
+class CartProducts extends StatelessWidget {
+  final CartController controller = Get.find();
+  CartProducts({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => SizedBox(
+          height: 300,
+          child: ListView.builder(
+            itemCount: controller.products.length,
+            itemBuilder: (BuildContext context, int index) {
+              return CartProductCard(
+                controller: controller,
+                product: controller.products.keys.toList()[index],
+                quantity: controller.products.values.toList()[index],
+                index: index,
+              );
+            },
+          )),
+    );
+  }
+}
+
+class CartProductCard extends StatelessWidget {
+  final CartController controller;
+  final Product product;
+  final int quantity;
+  final int index;
+
+  const CartProductCard({
+    Key? key,
+    required this.controller,
+    required this.product,
+    required this.quantity,
+    required this.index,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          CircleAvatar(
+            radius: 40,
+            backgroundImage: NetworkImage(
+              product.imageUrl,
+            ),
+          ),
+          const SizedBox(width: 20),
+          Expanded(child: Text(product.name)),
+          Expanded(child: Text(product.price.toString())),
+          IconButton(
+              onPressed: () {
+                controller.addProduct(product);
+              },
+              icon: const Icon(Icons.add_circle_outline)),
+          IconButton(
+              onPressed: () {
+                controller.removeProduct(product);
+              },
+              icon: const Icon(Icons.remove_circle_outline)),
+          Text(quantity.toString()),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/cart_total.dart
+++ lib/widgets/cart_total.dart
@@ -1,36 +1,35 @@
-// import 'package:flutter/widgets.dart';
-// import 'package:get/get.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
-// import '../controllers/cart_controller.dart';
+import '../controllers/cart_controller.dart';
 
-// class CartTotal extends StatelessWidget {
-//   final CartController controller = Get.find();
-//   CartTotal({super.key});
+class CartTotal extends StatelessWidget {
+  final CartController controller = Get.find();
+  CartTotal({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Obx(
-//       () => Container(
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//           children: [
-//             const Text(
-//               'Total',
-//               style: TextStyle(
-//                 fontSize: 20,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//             Text(
-//               'GHC ${controller.total}',
-//               style: const TextStyle(
-//                 fontSize: 20,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            'Total',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            'GHC ${controller.total}',
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+ 
